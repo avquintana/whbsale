f02370c6e8420c343c46c16fa63c83d92cd15ca5
Arlety <avquintana91@gmail.com>
2020-01-14 10:22:06 -0500

    prestashop logic corrections

--- src/api/prestashop.js
+++ src/api/prestashop.js
@@ -1,12 +1,30 @@
 const api = require('prestashop-sdk-16');
 
+const getCombinationByReference = (Presta, reference) => {
+    return new Promise((resolve, reject) => {
+        opt = {
+            filter: {
+                reference
+            },
+            display: '[id,id_product]'
+        }
+        Presta.get('combinations', opt).then(function (response) {
+            console.log(response);
+            resolve(response.prestashop.combinations);
+        }).catch(function (errors) {
+            reject(errors);
+        });
+    });
+}
+
 const setProductQuantityVersion16 = (stocks, company, reference) => {
     return new Promise((resolve, reject) => {
         // var Presta = new api({
         //     connection: `https://${company.webservice}@${url.hostname}`
         // })
-        var Presta = new api({ 
-            storeUrl: company.url,
+        var Presta = new api({
+            storeUrl: company.url[company.url.length - 1] === '/' ?
+                company.url.substr(0, company.url.length - 1) : company.url,
             apiKey: company.webservice
         });
         var opt = {};
@@ -23,18 +41,33 @@ const setProductQuantityVersion16 = (stocks, company, reference) => {
                 }
             }
         }
-        Presta.get('products', opt).then(function (response) {
-            var id = +response.prestashop.products.product.$.id;
+        Presta.get('products', opt).then(async function (response) {
+            var id;
+            try {
+                id = response.prestashop.products.product.$.id;
+            } catch (error) {
+                console.log(error);
+            }
+            var attributeId = 0;
+            // if (!id) {
+                var response = await getCombinationByReference(Presta, reference);
+            // }
             if (id) {
                 opt = {
                     id: id
                 }
-                Presta.get('products', opt).then(function (response) {
-                    // TODO: implement update request
+                Presta.edit('products', { price: 0 }, opt).then(function (response) {
                     console.log(response);
                 }).catch(function (errors) {
                     console.log(errors);
                 });
+                // Presta.get('products', opt).then(function (response) {
+                //     // TODO: implement update request
+                //     console.log(response);
+
+                // }).catch(function (errors) {
+                //     console.log(errors);
+                // });
             }
         }).catch(function (errors) {
             console.log(errors);
